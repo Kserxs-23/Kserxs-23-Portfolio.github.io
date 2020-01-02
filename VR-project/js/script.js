c5fa9ca1a3d0aa515aa6cb20cac7aaea3fd86121
Kserxs-23 <53793153+Kserxs-23@users.noreply.github.com>
2020-01-02 11:23:19 +0300

    VR-project_v.1

---
+++ VR-project/js/script.js
@@ -0,0 +1,365 @@
+
+
+/*stopping video after closing modal window*/
+$(document).ready(function() {
+	'use strict';
+
+	const close_btn = document.getElementById('pause_video');
+  const close_btn_2 = document.getElementById('pause_video_2');
+
+	const click = close_btn.addEventListener('click', ()=> {
+
+
+    var my_video = $(".watch-the-video-youtube");
+    var my_video = $("iframe")[0].contentWindow;
+
+    $("#pause_video").on("click", function() {
+        
+          my_video.postMessage('{"event": "command", "func": "pauseVideo", "args": ""}', "*");
+          
+        });
+
+  })
+
+  const click_2 = close_btn_2.addEventListener('click', ()=> {
+
+
+    var my_video = $(".watch-the-video-youtube");
+    var my_video = $("iframe")[0].contentWindow;
+
+    $("#pause_video_2").on("click", function() {
+        
+          my_video.postMessage('{"event": "command", "func": "pauseVideo", "args": ""}', "*");
+          
+        });
+
+  })
+
+
+
+});
+
+/*forms appearing*/
+
+$(document).ready(function() {
+
+        $('.header-link__order-link').on('click', function(event) {
+           event.preventDefault();
+           $('.request-display').addClass('request-block');
+           $('html').css('overflow', 'hidden');
+        });
+
+        $('.ask-manager').on('click', function(event) {
+          event.preventDefault();
+          $('.ask-display').addClass('ask-block');
+          $('html').css('overflow', 'hidden');
+        });
+
+        $('.main-trigger-content__btn').on('click', function(event) {
+          event.preventDefault();
+          $('.order-display').addClass('order-block');
+          $('html').css('overflow', 'hidden');
+        });
+
+        $('.games-btn').on('click', function(event) {
+          event.preventDefault();
+          $('.order-display').addClass('order-block');
+          $('html').css('overflow', 'hidden');
+        });
+
+        $('.about-btn').on('click', function(event) {
+          event.preventDefault();
+          $('.choose-display').addClass('choose-block');
+          $('html').css('overflow', 'hidden');
+        });
+
+        $('.form-select').focusin(function() {
+          $('.wrap-arrow').addClass('wrap-arrow-focus');
+        });
+        $('.form-select').focusout(function() {
+          $('.wrap-arrow').removeClass('wrap-arrow-focus');
+        });
+
+        $('.close-btn-icon').on('click', function(event) {
+          event.preventDefault();
+          $('.request-display').removeClass('request-block');
+          $('.ask-display').removeClass('ask-block');
+          $('.order-display').removeClass('order-block');
+          $('.choose-display').removeClass('choose-block');
+          $('input[name="user_phone"]').val("");
+          $('input[name="user_name"]').val("");
+          $('html').css('overflow', 'scroll');
+        });
+
+        $('.thank-btn').on('click', function(event) {
+          event.preventDefault();
+          $('.thank').css('display', 'none');
+          $('html').css('overflow', 'scroll');
+  });
+});
+
+
+/*send message and after submitting effects*/
+$(document).ready(function() {
+
+  $('#call-request').trigger('reset');
+  $(function() {
+    'use strict';
+    $('#call-request').on('submit', function(e) {
+      e.preventDefault();
+      $.ajax({
+        url: '../mailer/send.php',
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        data: new FormData(this),
+        success: function(msg) {
+          console.log(msg);
+          if (msg == 'ok') {
+            alert('Сообщение отправлено');
+            $('#call-request').trigger('reset'); // очистка формы
+            $('.request-display').removeClass('request-block');
+            $('.thank').css({
+              display: 'block',
+              overflow: 'hidden'
+            });
+          } else {
+            alert('Ошибка');
+          }
+        }
+      });
+    });
+  });
+
+  $('#get-answers').trigger('reset');
+  $(function() {
+    'use strict';
+    $('#get-answers').on('submit', function(e) {
+      e.preventDefault();
+      $.ajax({
+        url: '../mailer/send.php',
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        data: new FormData(this),
+        success: function(msg) {
+          console.log(msg);
+          if (msg == 'ok') {
+            alert('Сообщение отправлено');
+            $('#get-answers').trigger('reset'); // очистка формы
+            $('.ask-display').removeClass('ask-block');
+            $('.thank').css({
+              display: 'block',
+              overflow: 'hidden'
+            });
+          } else {
+            alert('Ошибка');
+          }
+        }
+      });
+    });
+  });
+
+  $('#order-vr').trigger('reset');
+  $(function() {
+    'use strict';
+    $('#order-vr').on('submit', function(e) {
+      e.preventDefault();
+      $.ajax({
+        url: '../mailer/send.php',
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        data: new FormData(this),
+        success: function(msg) {
+          console.log(msg);
+          if (msg == 'ok') {
+            alert('Заявка подана');
+            $('#order-vr').trigger('reset'); // очистка формы
+            $('.order-display').removeClass('order-block');
+            $('.thank').css({
+              display: 'block',
+              overflow: 'hidden'
+            });
+          } else {
+            alert('Ошибка');
+          }
+        }
+      });
+    });
+  });
+
+  $('#call-back-1').trigger('reset');
+    $(function() {
+      'use strict';
+      $('#call-back-1').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+          url: '../mailer/send.php',
+          type: 'POST',
+          contentType: false,
+          processData: false,
+          data: new FormData(this),
+          success: function(msg) {
+            console.log(msg);
+            if (msg == 'ok') {
+              alert('Заявка подана');
+              $('#call-back-1').trigger('reset'); // очистка формы
+              $('.thank').css({
+                display: 'block',
+                overflow: 'hidden'
+              });
+            } else {
+              alert('Ошибка');
+            }
+          }
+        });
+      });
+    });
+
+    $('#call-back-2').trigger('reset');
+    $(function() {
+      'use strict';
+      $('#call-back-2').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+          url: '../mailer/send.php',
+          type: 'POST',
+          contentType: false,
+          processData: false,
+          data: new FormData(this),
+          success: function(msg) {
+            console.log(msg);
+            if (msg == 'ok') {
+              alert('Заявка подана');
+              $('#call-back-2').trigger('reset'); // очистка формы
+              $('.thank').css({
+                display: 'block',
+                overflow: 'hidden'
+              });
+            } else {
+              alert('Ошибка');
+            }
+          }
+        });
+      });
+    });
+
+    $('#call-back-3').trigger('reset');
+    $(function() {
+      'use strict';
+      $('#call-back-3').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+          url: '../mailer/send.php',
+          type: 'POST',
+          contentType: false,
+          processData: false,
+          data: new FormData(this),
+          success: function(msg) {
+            console.log(msg);
+            if (msg == 'ok') {
+              alert('Заявка подана');
+              $('#call-back-3').trigger('reset'); // очистка формы
+              $('.thank').css({
+                display: 'block',
+                overflow: 'hidden'
+              });
+            } else {
+              alert('Ошибка');
+            }
+          }
+        });
+      });
+    });
+
+    $('#call-back-4').trigger('reset');
+    $(function() {
+      'use strict';
+      $('#call-back-4').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+          url: '../mailer/send.php',
+          type: 'POST',
+          contentType: false,
+          processData: false,
+          data: new FormData(this),
+          success: function(msg) {
+            console.log(msg);
+            if (msg == 'ok') {
+              alert('Заявка подана');
+              $('#call-back-4').trigger('reset'); // очистка формы
+              $('.thank').css({
+                display: 'block',
+                overflow: 'hidden'
+              });
+            } else {
+              alert('Ошибка');
+            }
+          }
+        });
+      });
+    });
+
+    $('#complectation').trigger('reset');
+    $(function() {
+      'use strict';
+      $('#complectation').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+          url: '../mailer/send-complectation.php',
+          type: 'POST',
+          contentType: false,
+          processData: false,
+          data: new FormData(this),
+          success: function(msg) {
+            console.log(msg);
+            if (msg == 'ok') {
+              alert('Заявка подана');
+              $('#complectation').trigger('reset'); // очистка формы
+              $('.choose-display').removeClass('choose-block');
+              $('.thank').css({
+                display: 'block',
+                overflow: 'hidden'
+              });
+            } else {
+              alert('Ошибка');
+            }
+          }
+        });
+      });
+    });
+
+
+});
+
+/*youtube block*/
+
+$(document).ready(function() {
+        
+  $('.video-word-link').on('click', function(event) {
+    event.preventDefault();
+    $('.watch-wrapper').css('display', 'block');
+    $('html').css('overflow', 'hidden');
+  });
+
+  $('.video-link').on('click', function(event) {
+    event.preventDefault();
+    $('.watch-wrapper').css('display', 'block');
+    $('html').css('overflow', 'hidden');
+  });
+
+  $('.watch-the-video').on('click', function(event) {
+    event.preventDefault();
+    $('.watch-wrapper').css('display', 'none');
+    ('html').css('overflow', 'scroll');
+  });    
+});
+
+/*masked input*/
+
+jQuery(function($){
+  $("input[name='user_phone']").mask("+ 9 (999) 999-99-99");
+});
+
+
---
+++ VR-project/mailer/send-complectation.php
@@ -0,0 +1,56 @@
+<?php
+// Файлы phpmailer
+require 'PHPMailer.php';
+require 'SMTP.php';
+require 'Exception.php';
+// Переменные, которые отправляет пользователь
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$complectation = $_POST['complectation'];
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    $msg = "ok";
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";                                          
+    $mail->SMTPAuth   = true;
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера GMAIL
+    $mail->Username   = 'gimadiev02@list.ru'; // Логин на почте
+    $mail->Password   = 'JUdevFeipGecDi2'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('gimadiev02@list.ru', "$name"); // Адрес самой почты и имя отправителя
+    // Получатель письма
+    $mail->addAddress('gimadiev02@list.ru');   // Ещё один, если нужен
+    // Прикрипление файлов к письму
+if (!empty($_FILES['myfile']['name'][0])) {
+    for ($ct = 0; $ct < count($_FILES['myfile']['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['myfile']['name'][$ct]));
+        $filename = $_FILES['myfile']['name'][$ct];
+        if (move_uploaded_file($_FILES['myfile']['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+        } else {
+            $msg .= 'Неудалось прикрепить файл ' . $uploadfile;
+        }
+    }   
+}
+        // -----------------------
+        // Само письмо
+        // -----------------------
+        $mail->isHTML(true);
+    
+        $mail->Subject = 'Заявка.';
+        $mail->Body    = "<b>Имя:</b> $name <br>
+                        <b>Телефон:</b> $phone <br>
+                        <b>Комплектация:</b> $complectation <br>";
+// Проверяем отравленность сообщения
+if ($mail->send()) {
+    echo "$msg";
+} else {
+echo "Сообщение не было отправлено. Неверно указаны настройки вашей почты";
+}
+} catch (Exception $e) {
+    echo "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
